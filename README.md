cb10df02ef1a23ccb1b7e4cd44de9125e12d885a
Gabriel Falcao <gabriel@nacaolivre.org>
2013-08-25 18:43:56 -0400

    documentation

---
+++ README.md
@@ -0,0 +1,22 @@
+# sure `1.2.2`
+
+A testing library for python with powerful and flexible assertions. Sure is heavily inspired by [should.js](https://github.com/visionmedia/should.js/)
+
+[![Build Status](https://travis-ci.org/gabrielfalcao/sure.png?branch=master)](https://travis-ci.org/gabrielfalcao/sure)
+
+
+# Installing
+
+    user@machine:~$ [sudo] pip install sure
+
+
+# Documentation
+
+Available in the [website](http://falcao.it/sure) or under the `spec` directory.
+
+You can also build the documentation locally using markment:
+
+```bash
+pip install markment
+markment --server --theme=merlot ./spec/
+```
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# <sure - utility belt for automated testing in python>
+# Copyright (C) <2010-2013>  Gabriel Falcão <gabriel@nacaolivre.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from sure import version
+from setuptools import setup
+
+
+def get_packages():
+    # setuptools can't do the job :(
+    packages = []
+    for root, dirnames, filenames in os.walk('sure'):
+        if '__init__.py' in filenames:
+            packages.append(".".join(os.path.split(root)).strip("."))
+
+    return packages
+
+setup(name='sure',
+    version=version,
+    description='utility belt for automated testing in python for python',
+    author=u'Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    url='http://github.com/gabrielfalcao/sure',
+    packages=get_packages(),
+    install_requires=[
+    ],
+)
